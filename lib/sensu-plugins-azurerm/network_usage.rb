cb6eda7483d7c1b0313b58561252f320269778e3
tombuildsstuff <git@ibuildstuff.co.uk>
2016-11-28 21:47:51 +0000

    Fixing linting issue in the Cores

--- lib/sensu-plugins-azurerm/network_usage.rb
+++ lib/sensu-plugins-azurerm/network_usage.rb
@@ -1,16 +1,13 @@
 module SensuPluginsAzureRM
-
   class NetworkUsage
-
     def buildVirtualNetworkClient(tenant_id, client_id, secret, subscription_id)
       token_provider = MsRestAzure::ApplicationTokenProvider.new(tenant_id, client_id, secret)
       credentials = MsRest::TokenCredentials.new(token_provider)
       client = Azure::ARM::Network::NetworkManagementClient.new(credentials)
       client.subscription_id = subscription_id
 
-      usageClient = Azure::ARM::Network::VirtualNetworkGatewayConnections.new(client)
-      usageClient
+      usage_client = Azure::ARM::Network::VirtualNetworkGatewayConnections.new(client)
+      usage_client
     end
-
   end
 end
--- lib/sensu-plugins-azurerm/servicebus_usage.rb
+++ lib/sensu-plugins-azurerm/servicebus_usage.rb
@@ -1,16 +1,13 @@
 module SensuPluginsAzureRM
-
   class ServiceBusUsage
-
     def buildServiceBusClient(tenant_id, client_id, secret, subscription_id)
       token_provider = MsRestAzure::ApplicationTokenProvider.new(tenant_id, client_id, secret)
       credentials = MsRest::TokenCredentials.new(token_provider)
       client = Azure::ARM::ServiceBus::ServiceBusManagementClient.new(credentials)
       client.subscription_id = subscription_id
 
-      usageClient = Azure::ARM::ServiceBus::Subscriptions.new(client)
-      usageClient
+      usage_client = Azure::ARM::ServiceBus::Subscriptions.new(client)
+      usage_client
     end
-
   end
 end
