061d7729ec612f491097d4fb5d625c5cc49525cc
tombuildsstuff <git@ibuildstuff.co.uk>
2016-12-02 17:55:34 +0000

    Hit it til it works

--- lib/sensu-plugins-azurerm.rb
+++ lib/sensu-plugins-azurerm.rb
@@ -1,3 +1,7 @@
+require 'azure_mgmt_compute'
+require 'azure_mgmt_network'
+require 'azure_mgmt_service_bus'
+
 require 'sensu-plugins-azurerm/common'
 require 'sensu-plugins-azurerm/compute_usage'
 require 'sensu-plugins-azurerm/network_usage'
