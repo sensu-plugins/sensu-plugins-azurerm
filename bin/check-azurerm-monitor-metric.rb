c7b75bafa91b398a9c1ef8a555b348bf4660e383
thomas.litton <thomas.litton@centurylink.com>
2018-01-26 17:18:20 -0600

    wip:  Implementing azure check

--- bin/check-azurerm-monitor-metric.rb
+++ bin/check-azurerm-monitor-metric.rb
@@ -6,6 +6,8 @@ require 'sensu-plugins-azurerm'
 class CheckAzurermMonitorMetric < Sensu::Plugin::Check::CLI
   include SensuPluginsAzureRM
 
+  AZURE_API_VER = "2017-05-01-preview"
+
   option :tenant_id,
          description: 'ARM Tenant ID. Either set ENV[\'ARM_TENANT_ID\'] or provide it as an option',
          short: '-t ID',
@@ -24,13 +26,14 @@ class CheckAzurermMonitorMetric < Sensu::Plugin::Check::CLI
          long: '--clientSecret SECRET',
          default: ENV['ARM_CLIENT_SECRET']
 
-  option :subscription_id,
-         description: 'ARM Subscription ID',
-         short: '-S ID',
-         long: '--subscription ID',
-         default: ENV['ARM_SUBSCRIPTION_ID']
+  # option :subscription_id,
+  #        description: 'ARM Subscription ID',
+  #        short: '-S ID',
+  #        long: '--subscription ID',
+  #        default: ENV['ARM_SUBSCRIPTION_ID']
 
-  option :resource_id,
+  # example id: /subscriptions/576b7196-d42b-4b63-b696-af3ff33269a7/resourceGroups/test-group-1/providers/Microsoft.Network/virtualNetworkGateways/test-gateway
+  option :resource,
          description:  'The (full) id of the resource for the metric',
          short: '-r ID',
          long: '--resource ID',
@@ -48,6 +51,12 @@ class CheckAzurermMonitorMetric < Sensu::Plugin::Check::CLI
          long: '--dimensions DIMENSIONS',
          proc: proc { |d| parse_dimensions(d) },
          default: []
+
+  option :aggregation,
+         description:  'Aggregation.  This can be Average, Count, Maximum, Minimum, Total',
+         short: '-a aggregation',
+         long: '--aggregation aggregation',
+         default: "average"
 
   option :warning_over,
          description: 'The warning threshold to check if the metric is forecasted to go over.',
@@ -81,6 +90,84 @@ class CheckAzurermMonitorMetric < Sensu::Plugin::Check::CLI
   end
 
   def run
-    ok "Test"
+    if config[:critical_over] && last_metric_value > config[:critical_over].to_f
+      critical "Metric #{config[:metric]} is #{last_metric_value}"
+    elsif config[:warning_over] && last_metric_value > config[:warning_over].to_f
+      warning "Metric #{config[:metric]} is #{last_metric_value}"
+    elsif config[:critical_under] && last_metric_value < config[:critical_under].to_f
+      critical "Metric #{config[:metric]} is #{last_metric_value}"
+    elsif config[:warning_under] && last_metric_value < config[:warning_under].to_f
+      warning "Metric #{config[:metric]} is #{last_metric_value}"
+    else
+      ok "Metric #{config[:metric]} is #{last_metric_value}"
+    end
+  end
+
+  def last_metric_value
+    @last_metric_value ||= get_last_metric_value
+  end
+
+  def get_last_metric_value
+    last_metric_value = nil
+
+    metric_response[:value].last[:timeseries].last[:data].reverse_each { |val|
+      if val[config[:aggregation].to_sym]
+        last_metric_value = val[config[:aggregation].to_sym].to_f
+        break
+      end
+    }
+    last_metric_value
+  end
+
+  def metric_response
+    provider = MsRestAzure::ApplicationTokenProvider.new(
+      config[:tenant_id],
+      config[:client_id],
+      config[:client_secret]
+    )
+
+    begin
+      res = config[:resource].start_with?("/") ? config[:resource] : "/" + config[:resource]
+      url = "https://management.azure.com#{res}/providers/microsoft.insights/metrics?" +
+        "api-version=#{AZURE_API_VER}&" +
+        "metric=#{config[:metric]}&" +
+        "timespan=#{CGI.escape(timespan)}&" +
+        "aggregation=#{config[:aggregation]}"
+
+      uri = URI.parse(url)
+
+      res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        req = Net::HTTP::Get.new(uri)
+        req["Authorization"] = provider.get_authentication_header
+        req["Content-Type"] = "application/json"
+        http.request(req)
+      end
+    rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET,
+      EOFError, Net::HTTPBadResponse,
+      Net::HTTPHeaderSyntaxError, Net::ProtocolError,
+      Errno::ECONNREFUSED => e
+
+      critical e
+    end
+
+    handle_response(res)
+
+    JSON.parse(res.body, symbolize_names: true)
+  end
+
+  def timespan
+    start_date = Time.now - 300 # 5 min
+    end_date = Time.now
+    "#{start_date.iso8601}/#{end_date.iso8601}"
+  end
+
+  def handle_response(res)
+    case res
+      when Net::HTTPSuccess then
+        return
+      else
+        critical "Failed to get metric:\n#{res.body}"
+    end
+
   end
 end
--- test/bin/check_azurerm_monitor_metric_spec.rb
+++ test/bin/check_azurerm_monitor_metric_spec.rb
@@ -5,23 +5,125 @@ require 'webmock/rspec'
 
 describe "check monitor metric script" do
   subject(:check_instance) { CheckAzurermMonitorMetric.new(script_args) }
-  let(:script_args) { [] }
-  let(:expected_url) { "http://localhost:8080/metrics" }
+  let(:script_args) { [
+    "--resource", resource_id,
+    "--metric", metric_name,
+    "--critical", critical
+  ] }
+  let(:resource_id) { "resource" }
+  let(:metric_name) { "metric" }
+  let(:critical) { "3.0" }
+
+  let(:expected_url) {
+    "https://management.azure.com/#{resource_id}/providers/microsoft.insights/metrics?" +
+      "api-version=2017-05-01-preview&" +
+      "metric=#{metric_name}&" +
+      "timespan=#{timespan}&" +
+      "aggregation=#{aggregation}"
+  }
+  let(:timespan) {
+    start_date = Time.now - 300
+    end_date = Time.now
+
+    "#{start_date.iso8601}/#{end_date.iso8601}"
+  }
+
+  let(:aggregation) { "average" }
+
   let(:mock_az_provider) { double }
+
+  let(:metric_resp) {
+    {
+      cost: 0.0,
+      interval: "0:01:00",
+      timespan: "2018-01-26T16:56:00Z/2018-01-26T17:56:00Z",
+      value: [
+        {
+          id: "/subscriptions/576b7196-d42b-4b63-b696-af3ff33269a7/resourceGroups/test-group-1/providers/Microsoft.Network/virtualNetworkGateways/test-gateway/providers/Microsoft.Insights/metrics/TunnelAverageBandwidth",
+          name: {
+            localizedValue: "Tunnel Bandwidth",
+            value: "TunnelAverageBandwidth"
+          },
+          resourceGroup: "test-group-1",
+          timeseries: [
+            {
+              data: [
+                {
+                  average: "2.2",
+                  timeStamp: "2018-01-26T16:56:00+00:00"
+                },
+                {
+                  average: value1,
+                  timeStamp: "2018-01-26T16:56:00+00:00"
+                },
+                {
+                  average: value2,
+                  timeStamp: "2018-01-26T16:57:00+00:00"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+
+  let(:value1) { "1.2" }
+  let(:value2) { "2.2" }
 
   before(:each) do
     mock_result_methods(check_instance)
-    allow(MsRestAzure::ApplicationTokenProvider).to receive(:get).and_return(mock_az_provider)
+    allow(MsRestAzure::ApplicationTokenProvider).to receive(:new).and_return(mock_az_provider)
+    allow(mock_az_provider).to receive(:get_authentication_header).and_return("TOKEN")
+    stub_request(:get, expected_url).to_return(body: JSON.dump(metric_resp))
+
   end
 
   context "When metric not over thresholds" do
     it "Returns ok" do
       check_instance.run
 
-      expect(instance).to have_received(:ok)
+      expect(check_instance).to have_received(:ok)
     end
 
-    it "Retrieves the metric to compare"
+    it "Retrieves the metric to compare" do
+      check_instance.run
+
+      expect(WebMock).to have_requested(:get, expected_url).with { |req|
+        expect(req.headers["Authorization"]).to eq("TOKEN")
+      }
+    end
+  end
+
+  context "When last metric value is null" do
+    let(:value1) { "-2.0" }
+    let(:value2) { nil }
+    let(:critical) { "-1.0" }
+    it "Uses the last entry with a value" do
+      check_instance.run
+
+      expect(check_instance).to have_received(:ok)
+    end
+  end
+
+  context "When no metric has a value" do
+    it "Returns an known response"
+  end
+
+  context "When response has multiple values" do
+    it "Uses the last value entry"
+  end
+
+  context "When response has no timeseries" do
+    it "Returns an known response"
+  end
+
+  context "When response has multiple time series" do
+    it "Uses the last timeseries entry"
+  end
+
+  context "When response has no data entry" do
+    it "Returns an known response"
   end
 
   context "When no tenant, client, secret, and subscription given" do
@@ -46,12 +148,19 @@ describe "check monitor metric script" do
     end
   end
 
+  context "When metric under thresholds given" do
+    context "And the metric lower the warning" do
+      it "Returns warning"
+    end
+
+    context "And the metric lower the critical" do
+      it "Returns a critical"
+    end
+  end
+
   def mock_result_methods(instance)
     allow(instance).to receive(:critical)
     allow(instance).to receive(:warning)
     allow(instance).to receive(:ok)
   end
-
 end
-
-
